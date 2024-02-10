cc847445a7ff57418a7752c4c76d90930c8ed6d0
keithb19 <keithjr500@gmail.com>
2024-02-10 01:18:07 -0600

    updated the react import

--- src/layout/HeaderAndFooter.tsx
+++ src/layout/HeaderAndFooter.tsx
@@ -1,6 +1,7 @@
 import Footer from "components/Footer/Footer";
 import Header from "components/Header/Header";
 import { Outlet } from "react-router-dom";
+import React from "react";
 
 export default function HeaderAndFooter() {
   return (
