524bc49bbe485c068f5cfd24c5bfadcd4260bb22
Nick <51724890+nickdep217@users.noreply.github.com>
2024-04-26 17:28:34 -0500

    fixed wrong link not found (test)

--- src/pages/ContestRedirects/SingleContestRedirect.tsx
+++ src/pages/ContestRedirects/SingleContestRedirect.tsx
@@ -12,47 +12,40 @@ export default function SingleContestRedirectPage() {
     const [message, setMessage] = React.useState('Redirecting...')
     const [device, setDevice] = React.useState('')
     document.title = 'Join Contest - Parlay Bingo'
+
     const tryRedirectSendToAppStoreOtherwise = async () => {
-        setDevice(navigator.userAgent);
-        if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
-            window.location.href = `parlaybingo://app/contest?id=${id}&tab=info`;
-            const timerId = setTimeout(function () {
-                setMessage('Redirecting you to the App Store...');
-                window.location.href = 'https://apps.apple.com/us/app/parlay-bingo/id1665470403';
-            }, 100);
-            window.onblur = function () {
-                clearTimeout(timerId);
-            };
-        }else{
-            //window.open('https://apps.apple.com/us/app/parlay-bingo-fantasy-sports/id1665470403', '_blank', 'noopener,noreferrer');
-            //window.location.href = 'https://apps.apple.com/us/app/parlay-bingo-fantasy-sports/id1665470403';
-            setTimeout(() => {
-                setMessage('Redirecting you to the App Store...');
-                window.location.href = 'https://apps.apple.com/us/app/parlay-bingo/id1665470403';
-            }, 100);
-        }
+        window.location.href = `parlaybingo://app/contest?id=${id}&tab=info`;
+        const timerId = setTimeout(function () {
+            setMessage('Redirecting you to the App Store...');
+            window.location.href = 'https://apps.apple.com/us/app/parlay-bingo/id1665470403';
+        }, 600);
+        window.onblur = function () {
+            clearTimeout(timerId);
+        }; 
     }
 
     useEffect(() => {
-            if(id){
-                axios.get(`${baseUrl}/contest?id=${id}`)
-                    .then(response => {
-                        if((response.data) && (response.data.contestName)){
-                            let contestName = response.data.contestName;
-                            document.title = contestName;
-                            console.log(response.data);
-                            tryRedirectSendToAppStoreOtherwise();
-                        }else{
-                            setMessage('Contest not found');
-                        }
-                    })
-                    .catch(error => {
-                    // Handle the error here
-                    console.error(error);
-                    });
-
+        const fetchData = async () => {
+          if (id) {
+            try {
+              const response = await axios.get(`${baseUrl}/contest?id=${id}`);
+              if (response.data && response.data.contestName) {
+                let contestName = response.data.contestName;
+                document.title = contestName;
+                console.log(response.data);
+                await tryRedirectSendToAppStoreOtherwise();
+              } else {
+                setMessage('Contest not found');
+              }
+            } catch (error) {
+              // Handle the error here
+              console.error(error);
             }
-        }, [id]);
+          }
+        };
+      
+        fetchData();
+      }, [id]);
 
   // Render a message while the new tab is being opened
   return (
