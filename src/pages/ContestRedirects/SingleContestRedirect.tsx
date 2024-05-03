8b9d7af7ad065d2d3645548465417bf4c029a472
Nick <51724890+nickdep217@users.noreply.github.com>
2024-05-03 14:10:46 -0500

    made redirect page look nice and created a fallback if redirect does not work

--- src/pages/ContestRedirects/SingleContestRedirect.tsx
+++ src/pages/ContestRedirects/SingleContestRedirect.tsx
@@ -75,10 +75,27 @@ export default function SingleContestRedirectPage() {
 
   // Render a message while the new tab is being opened
   return (
-    <>
-        <div>{message}</div>
-        <div>{device}</div>
-    </>
-  );
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1 style={{ marginBottom: '20px' }}>{message}</h1>
+      <p>{device}</p>
+      <a
+        href="https://apps.apple.com/us/app/parlay-bingo/id1665470403"
+        style={{
+          display: 'inline-block',
+          backgroundColor: '#007BFF',
+          color: 'white',
+          padding: '10px 20px',
+          textDecoration: 'none',
+          borderRadius: '5px',
+          marginTop: '20px',
+        }}
+      >
+        Download Parlay Bingo
+      </a>
+      <p style={{ color: 'grey', marginTop: '10px' }}>
+        If the redirect does not work, download the app an retry.
+      </p>
+    </div>
+  )
 }
 
